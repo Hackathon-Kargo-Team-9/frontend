ffb70fc8ba6a85c2ad58916d3d1864374842f181
Bintang Fajarianto <13519138@std.stei.itb.ac.id>
2022-08-13 11:40:57 +0700

    init allocate shipment no style

--- pages/shipment/allocate.js
+++ pages/shipment/allocate.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+`;
 
 function AllocateShipment() {
-  return <div>AllocateShipment</div>;
+
+  const [truck, setTruck] = useState("");
+  const [driver, setDriver] = useState("");
+
+  const handleCancel = {}
+  const handleAllocate = {}
+
+  return (
+    <Container>
+      <label>Truck</label>
+      <input 
+        type="text"
+        value={truck}
+        onChange={(e) => setTruck(e.target.value)}
+      />
+      <label>Driver</label>
+      <input 
+        type="text"
+        value={driver}
+        onChange={(e) => setDriver(e.target.value)}
+      />
+
+  <button onClick={handleAllocate}>
+    Allocate
+  </button>
+  <button onClick={handleCancel}>
+    Cancel
+  </button>
+
+    </Container>
+  );
 }
 
 export default AllocateShipment;
