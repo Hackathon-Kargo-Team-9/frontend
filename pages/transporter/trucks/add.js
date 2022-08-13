25acc34d2a55e4511cd62d9e2b27ffbb4ab62709
syafiqabdillah <abdillah.syafiq@gmail.com>
2022-08-13 11:40:23 +0700

    initiate add truck page

--- pages/transporter/trucks/add.js
+++ pages/transporter/trucks/add.js
@@ -1,7 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-function AddTruck() {
-  return <div>AddTruck</div>;
+const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  background-color: var(--light);
+  color: var(--darkgrey);
+  max-width: 500px;
+  margin: 0 auto;
+`;
+
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const FormRow = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const Label = styled.label`
+  width: 150px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 5px 10px;
+`;
+
+function AddTrucks() {
+  const [licenseNumber, setLicenseNumber] = useState("");
+  const [licenseType, setLicenseType] = useState("");
+  const [truckType, setTruckType] = useState("");
+  const [productionYear, setProductionYear] = useState("");
+
+  return (
+    <Container>
+      <Form>
+        <FormRow>
+          <Label htmlFor="license">License Number</Label>
+          <Input
+            type="text"
+            value={licenseNumber}
+            onChange={(e) => setLicenseNumber(e.target.value)}
+          />
+        </FormRow>
+        <FormRow>
+          <Label htmlFor="license">License Type</Label>
+          <Input
+            type="text"
+            value={licenseType}
+            onChange={(e) => setLicenseType(e.target.value)}
+          />
+        </FormRow>
+        <FormRow>
+          <Label htmlFor="license">Truck Type</Label>
+          <Input
+            type="text"
+            value={truckType}
+            onChange={(e) => setTruckType(e.target.value)}
+          />
+        </FormRow>
+        <FormRow>
+          <Label htmlFor="license">Production Year</Label>
+          <Input
+            type="text"
+            value={productionYear}
+            onChange={(e) => setProductionYear(e.target.value)}
+          />
+        </FormRow>
+        <button>submit</button>
+      </Form>
+    </Container>
+  );
 }
 
-export default AddTruck;
+export default AddTrucks;
