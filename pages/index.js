f7f2be73a8c72d1bc68ecbe66d50fb870d80eecb
syafiqabdillah <abdillah.syafiq@gmail.com>
2022-08-13 12:46:50 +0700

    prettify login page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -8,27 +5,53 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 200px;
+  height: 100vh;
 `;
 
 const Container2 = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 30px;
+  padding: 20px;
+  border: 1px solid var(--grey);
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+
+const InputRow = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
 `;
 
 const Radio = styled.div`
   display: flex;
   flex-direction: column;
+  font-size: 20px;
+`;
+
+const Img = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: brightness(0.4);
 `;
 
 export default function Home() {
   return (
     <Container>
+      <Img
+        src="https://images.unsplash.com/photo-1587293852726-70cdb56c2866?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80"
+        alt="gudang"
+      />
       <Container2>
         <p>Role</p>
         <Radio>
-          <div>
+          <InputRow>
             <input
               id="transporter"
               value="transporter"
@@ -36,11 +59,11 @@ export default function Home() {
               type="radio"
             />
             Transporter
-          </div>
-          <div>
+          </InputRow>
+          <InputRow style={{ marginTop: "5px" }}>
             <input id="Shipper" value="Shipper" name="user" type="radio" />
             Shipper
-          </div>
+          </InputRow>
         </Radio>
       </Container2>
       <button>LOGIN</button>
